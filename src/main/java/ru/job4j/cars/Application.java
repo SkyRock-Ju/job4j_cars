ecfb4daac25bfb22de91b6386bb6e336b39bcaa7
Dulustan <skyrockpk@gmail.com>
2023-04-17 17:49:31 +0700

    updated file structure

--- src/main/java/ru/job4j/cars/Application.java
+++ src/main/java/ru/job4j/cars/Application.java
@@ -1,4 +1,4 @@
-package ru.job4j.cars.ru.job4j.cars;
+package ru.job4j.cars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ru/job4j/cars/ApplicationTests.java
+++ src/test/java/ru/job4j/cars/ApplicationTests.java
@@ -1,4 +1,4 @@
-package ru.job4j.cars.ru.job4j.cars;
+package ru.job4j.cars;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
