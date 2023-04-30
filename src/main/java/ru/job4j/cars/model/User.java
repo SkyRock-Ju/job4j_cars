f6e5e0d2b37742c4a16c8f9ed52a140b8d6a27d1
Dulustan <skyrockpk@gmail.com>
2023-04-30 23:31:16 +0700

    edit User.java to hibernate type entity

--- src/main/java/ru/job4j/cars/model/User.java
+++ src/main/java/ru/job4j/cars/model/User.java
@@ -1,9 +1,14 @@
 package ru.job4j.cars.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
+@Table(name = "auto_users")
 @Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String login;
     private String password;
