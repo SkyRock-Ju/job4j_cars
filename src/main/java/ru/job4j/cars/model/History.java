8499b4e67ae2bce6b8d17a0670f858d72ba2585c
Dulustan <skyrockpk@gmail.com>
2023-05-24 21:57:41 +0700

    added ManyToMany relation to History.java model

--- src/main/java/ru/job4j/cars/model/History.java
+++ src/main/java/ru/job4j/cars/model/History.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -17,4 +19,11 @@ public class History {
     private int id;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "history_owner", joinColumns = {
+            @JoinColumn(name = "history_id", nullable = false, updatable = false)},
+            inverseJoinColumns = {
+                    @JoinColumn(name = "car_id", nullable = false, updatable = false)})
+    private List<Car> cars = new ArrayList<>();
 }
